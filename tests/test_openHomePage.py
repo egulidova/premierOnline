33995d5d4b61cf956df78095b7636e2dd3c7e111
Olena Rutkovska <gulidova.elena@gmail.com>
2026-07-16 19:25:32 +0300

    fix register tests

--- tests/test_openHomePage.py
+++ tests/test_openHomePage.py
@@ -8,6 +8,19 @@ def home_page(page):
     page.goto(BASE_URL)
     return page
 
+@pytest.mark.parametrize(
+    "name, href",
+    [
+        ("Events", "https://www.premieronline.com/calendar"),
+        ("Ratings", "https://www.premieronline.com/event_ratings.php"),
+        ("Help", "https://www.premieronline.com/help"),
+        ("عربى", "https://www.premieronline.com/action/lang.php?lang=ar"),
+    ],
+)
+
+def test_navigation_menu(home_page, name, href):
+    link = home_page.get_by_role("link", name=name, exact=True)
+
 def test_open_home_page(home_page):
 
     expect(home_page).to_have_url("https://www.premieronline.com/")
@@ -26,30 +39,30 @@ def test_check_logo(home_page):
 
 def test_check_sign_in(home_page):
 
-    sign_in_button = home_page.locator("a[href='https://www.premieronline.com/action/dologin']").first
+    sign_in_button = home_page.locator("a[href='https://www.premieronline.com/action/dologin']").first()
     expect(sign_in_button).to_be_visible()
     expect(sign_in_button).to_have_attribute("href","https://www.premieronline.com/action/dologin")
 
-def test_check_menu_events(home_page):
-
-    events = home_page.get_by_text("Events").nth(0)
-    expect(events).to_be_visible()
-    expect(events).to_have_attribute("href", "https://www.premieronline.com/calendar")
-
-def test_check_menu_ratings(home_page):
+# def test_check_menu_events(home_page):
     
-    ratings = home_page.get_by_text("Ratings").nth(0)
-    expect(ratings).to_be_visible()
-    expect(ratings).to_have_attribute("href", "https://www.premieronline.com/event_ratings.php")
+#     events_link = home_page.get_by_text("Events").nth(0)
+#     expect(events_link).to_be_visible()
+#     expect(events_link).to_have_attribute("href", href)
 
-def test_check_menu_help(home_page):
+# def test_check_menu_ratings(home_page):
     
-    help = home_page.get_by_text("Help").nth(0)
-    expect(help).to_be_visible()
-    expect(help).to_have_attribute("href", "https://www.premieronline.com/help")
+#     ratings_link = home_page.get_by_text("Ratings").nth(0)
+#     expect(ratings_link).to_be_visible()
+#     expect(ratings_link).to_have_attribute("href", "https://www.premieronline.com/event_ratings.php")
 
-def test_check_menu_lang(home_page):
+# def test_check_menu_help(home_page):
     
-    lang = home_page.get_by_text("عربى").nth(0)
-    expect(lang).to_be_visible()
-    expect(lang).to_have_attribute("href", "https://www.premieronline.com/action/lang.php?lang=ar")
+#     help_link = home_page.get_by_text("Help").nth(0)
+#     expect(help_link).to_be_visible()
+#     expect(help_link).to_have_attribute("href", "https://www.premieronline.com/help")
+
+# def test_check_menu_lang(home_page):
+    
+#     lang_link = home_page.get_by_text("عربى").nth(0)
+#     expect(lang_link).to_be_visible()
+#     expect(lang_link).to_have_attribute("href", "https://www.premieronline.com/action/lang.php?lang=ar")
